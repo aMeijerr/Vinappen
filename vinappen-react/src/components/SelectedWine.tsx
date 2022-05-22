6dfbf4c5402a1291326c6906e156b86615654d2a
Alex Meijer <macmeijer@MacMeijer.local>
2022-05-22 10:56:11 +0200

    Add conditional rendering to star rating

--- vinappen-react/src/components/SelectedWine.tsx
+++ vinappen-react/src/components/SelectedWine.tsx
@@ -48,7 +48,7 @@ console.log(wine.ratings)
                 </div>
                 <div className="wine-rating">
                 <WineRating wineId={wine._id}/>
-                <p>{avgSum}%</p>
+                <p>{avgSum.toFixed()}%</p>
                 </div>
 
                 <div className="winemaking-description">
--- vinappen-react/src/components/WineRating.tsx
+++ vinappen-react/src/components/WineRating.tsx
@@ -13,14 +13,31 @@ export default function WineRating({wineId}: RatingProps) {
     await postRating(wineId, rate)
     console.log(rate)
   }
-//add onClick show/hide rating/text
-  return (
-    <div className='App'>
+
+  const renderStarRating = () => {
+    if (rating === 0) {
+      return <div className='App'>
       <Rating 
       onClick={handleRating}
       ratingValue={rating} 
       fillColorArray={['#f17a45', '#f19745', '#f1a545', '#f1b345', '#f1d045']}
       />
     </div>
+    } else {
+      return <h3>Thank you for voting!</h3>;
+    }
+  }
+//add onClick show/hide rating/text
+  return (
+    <>
+    {renderStarRating()}
+    </>
+    // <div className='App'>
+    //   <Rating 
+    //   onClick={handleRating}
+    //   ratingValue={rating} 
+    //   fillColorArray={['#f17a45', '#f19745', '#f1a545', '#f1b345', '#f1d045']}
+    //   />
+    // </div>
   )
 }
